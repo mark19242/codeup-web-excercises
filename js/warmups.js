36606d19b709b67d6e06c6cb2d2e2b89b2c8f59b
Mark Smith <Mark19242@gmail.com>
2023-11-07 10:14:49 -0600

    dom-query

--- js/warmups.js
+++ js/warmups.js
@@ -84,27 +84,27 @@
 
 
 
-const fizzBuzz = (start,end) => {
-    start = parseFloat(start);
-    end = parseFloat(end);
-    if (isNaN(start) || isNaN(end)) {
-        return false;
-    }
-    for (let i = 1; i <= 100 ; i++) {
-        if (i % 3 === 0 && i % 5===0) {
-            console.log("FizzBuzz")
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else if (i % 3=== 0) {
-            console.log("Fizz")
-        }
-    }
-
-
-};
-
-// Call the function to perform FizzBuzz for a specific range
-fizzBuzz(1, 100);
+// const fizzBuzz = (start,end) => {
+//     start = parseFloat(start);
+//     end = parseFloat(end);
+//     if (isNaN(start) || isNaN(end)) {
+//         return false;
+//     }
+//     for (let i = 1; i <= 100 ; i++) {
+//         if (i % 3 === 0 && i % 5===0) {
+//             console.log("FizzBuzz")
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else if (i % 3=== 0) {
+//             console.log("Fizz")
+//         }
+//     }
+//
+//
+// };
+//
+// // Call the function to perform FizzBuzz for a specific range
+// fizzBuzz(1, 100);
 
 
 // Write your FizzBuzz logic here
@@ -114,3 +114,86 @@ fizzBuzz(1, 100);
 // Check if the current number is divisible by 5
 // console.log "Buzz" if it is divisible by 5
 // If the number is not divisible by 3 or 5, console.log the number itself
+
+
+// const findLongestWord = (sentence) => {
+//     // Step 1: Split the sentence into an array of words
+//     const words = sentence.split(" ");
+//
+//     // Step 2: Initialize the variable to keep track of the longest word
+//     let longestWord = "";
+//
+//     // Step 3: Iterate through the array of words
+//     for (const word of words) {
+//         // Compare the length of the current word with the length of the longest word
+//         if (word.length > longestWord.length) {
+//             // Update the longest word if the current word is longer
+//             longestWord = word;
+//         }
+//     }
+//
+//     // Step 4: Return the longest word found
+//     return longestWord;
+// };
+//
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // Output: jumped
+
+const createPairs = (arr) => {
+    if (students.length % 2 != 0) {
+
+    } else {
+        let arr1 = students.slice(), arr2 = students.slice();
+
+        arr1.sort(function () {
+            return 0.5 - Math.random();
+        });
+        arr2.sort(function () {
+            return 0.5 - Math.random();
+        });
+
+        while (arr1.length) {
+            let name1 = arr1.pop(),
+                name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
+
+
+            console.log(name1 + ", " + name2);
+        }
+    }
+}
+
+const students = [
+    "Amanda",
+    "Bianca",
+    "Cameron",
+    "Chris",
+    "Dany",
+    "David",
+    "Diana",
+    "Drew",
+    "Evan",
+    "Hassan",
+    "Jack",
+    "Jason",
+    "John",
+    "Jonathan",
+    "Jordan",
+    "Joshua",
+    "Katie",
+    "Luis",
+];
+
+console.log(createPairs(students));
+// Expected output (pairs will be different each time):
+/*
+[
+    [ 'Amanda', 'Bianca' ],
+    [ 'Cameron', 'Chris' ],
+    [ 'Dany', 'David' ],
+    [ 'Diana', 'Drew' ],
+    [ 'Evan', 'Hassan' ],
+    [ 'Jack', 'Jason' ],
+    [ 'John', 'Jonathan' ],
+    [ 'Jordan', 'Joshua' ],
+    [ 'Katie', 'Luis' ]
+]
+*/
